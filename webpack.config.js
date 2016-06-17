7af8737a4bc64505bc8489e89c5333731c71557b
Nick Meldrum <nick@nickmeldrum.com>
2016-06-17 16:16:56 +0100

    step 8: react hot loading

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,22 @@
 'use strict';
+var path = require('path');
 
 module.exports = {
-    entry: './src/index.js',
+    entry: [
+        'webpack-dev-server/client?http://0.0.0.0:8080',
+        'webpack/hot/only-dev-server',
+        './src/index.js'
+    ],
     output: {
         filename: './bin/app.bundle.js'
     },
     module: {
         loaders: [
-            { test: /\.js$/, loader: "babel" }
+            {
+                test: /\.js$/,
+                loaders: ['react-hot', 'babel'],
+                include: path.join(__dirname, 'src')
+            }
         ]
     },
     devtool: 'source-map'
