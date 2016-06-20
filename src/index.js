5e00b48ba83b8cc4c9b513e6ad98bf2131ef2eee
Nick Meldrum <nick@nickmeldrum.com>
2016-06-20 16:42:39 +0100

    step 14: hook up redux dev tools

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, compose } from 'redux';
 import documentStore from './reducers/document.js';
 
 import Document from './containers/document.js';
 
-let store = createStore(documentStore)
+let store = createStore(documentStore, {parts: []}, compose(
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+))
 
 ReactDOM.render(
     <Provider store={store}>
