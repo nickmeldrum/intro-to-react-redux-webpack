b796257fc2c7c32ee118ccf9a7b22dfe2b5f955a
Nick Meldrum <nick@nickmeldrum.com>
2016-06-21 11:31:30 +0100

    step 24: end 2 end testing with server interaction

--- e2e-tests/spec.js
+++ e2e-tests/spec.js
@@ -26,5 +26,23 @@ describe('Document', function() {
             client.expect.element('body').text.to.contain(testData.partName).before(1000);
         });
     });
+
+    describe('Document management', function() {
+        it('clicking on document and renaming it changes the document title', function(client) {
+            client.url('http://localhost:8080/')
+                .expect.element('body').to.be.present.before(1000);
+
+            client.perform((client, done) => {
+                console.log('[LOG:] renaming document with title: ', testData.documentName);
+                done();
+            });
+
+            client.click('h1');
+
+            client.setValue('input[type=text]', [testData.documentName, client.Keys.ENTER]);
+
+            client.expect.element('h1').text.to.contain(testData.documentName).before(2500);
+        });
+    });
 });
 
